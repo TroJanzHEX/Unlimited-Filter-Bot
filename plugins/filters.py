81d333ca29c510c35cac9f7c1c1c303e3580e435
๓คгςยร <pranav.nss6@gmail.com>
2021-03-21 16:22:40 +0530

    text edit

--- plugins/filters.py
+++ plugins/filters.py
@@ -292,7 +292,10 @@ async def delall(client, message):
                 await message.reply_text("Make sure I'm present in your group!!", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text(
+                "I'm not connected to any groups!\nCheck /connections or connect to any groups",
+                quote=True
+            )
             return
 
     elif (chat_type == "group") or (chat_type == "supergroup"):
