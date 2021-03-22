8c2faa6e46c1840a3d575db202f38c1a6c90ca12
๓คгςยร <pranav.nss6@gmail.com>
2021-03-23 00:14:44 +0530

    added help message

--- script.py
+++ script.py
@@ -13,7 +13,42 @@ See <i>help</i> for commands and more details.
 © @TroJanzHEX</b>"""
 
 
-    HELP_MSG = """soon"""
+    HELP_MSG = """
+<i>Add me as admin in your group and start filtering :)</i>
+
+
+<b>Basic Commands;</b>
+
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+
+<b>Filter Commands;</b>
+
+<code>/add name reply</code>  -  Add filter for name
+
+<code>/del name</code>  -  Delete filter
+
+<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+
+<code>/viewfilters</code>  -  List all filters in chat
+
+
+<b>Connection Commands;</b>
+
+<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<code>/connect</code> in groups.
+
+<code>/connections</code>  -  Manage your connections.
+
+
+<b>Extras;</b>
+
+/id  -  Shows ID information
+
+<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
