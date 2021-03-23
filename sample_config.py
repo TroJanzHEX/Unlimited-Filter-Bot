b40644a73003bb456b7c80a6e3b41688fe10efed
๓คгςยร <pranav.nss6@gmail.com>
2021-03-23 12:26:54 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,14 +2,20 @@ import os
 
 class Config(object):
 
+    # Get a bot token from botfather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
+    # Get from my.telegram.org (or @UseTGXBot)
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
+    # Get from my.telegram.org (or @UseTGXBot)
     API_HASH = os.environ.get("API_HASH", "")
     
+    # Database URL from https://cloud.mongodb.com/
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
 
+    # Your database name from mongoDB
     DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "Cluster0"))
 
+    # ID of users that can use the bot commands
     AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
