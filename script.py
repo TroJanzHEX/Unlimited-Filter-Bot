467375f15ad4f37309d969927a8d5ab3a1b7880f
bharathcsmys <79088828+bharathcsmys@users.noreply.github.com>
2021-03-27 15:46:37 +0530

    Abc.py
    Haa

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b> @ELEVENO_ELDORADO</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name :Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @SINCE_14C5   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
