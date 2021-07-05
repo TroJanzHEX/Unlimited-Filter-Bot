4f14f0d3046d8731f3843afc4cb13d0fc274db98
Muthalib <85754819+abdullaalmuthalib@users.noreply.github.com>
2021-07-05 06:22:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -217,7 +217,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Command Help", callback_data="help_data")
+                    InlineKeyboardButton("🔰Group🔰", url="https://t.me/MOVIE_ARCADE")
                 ]
             ]
         ),
@@ -243,12 +243,10 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    
                 ]
             ]
         ),
@@ -264,8 +262,7 @@ async def about(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                    
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
