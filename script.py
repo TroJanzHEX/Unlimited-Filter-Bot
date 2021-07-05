803ed4a214980ff5499cb543c6684cbad09eaac3
Muthalib <85754819+abdullaalmuthalib@users.noreply.github.com>
2021-07-05 06:19:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,62 +2,11 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
-
-See <i>/help</i> for commands and more details.</b>
+ഞാൻ ഒരു ബോട്ട് ആണ്, എൻറെ വിധി എൻറെ [അഡ്മിന്റെ](https://t.me/nokiyirunno_ippo_kittum) തീരുമാനങ്ങളാണ്,അപ്പൊ വെറുതെ സമയം കളയണ്ട .</b>
 """
 
 
-    HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
+    HELP_MSG = """<b>NO ONE GONNA HELP YOU.</b>"""
 
 
-<b>Basic Commands;</b>
-
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @TroJanzHEX</b>
-"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
-
-⭕️<b>Creater :</b> @TroJanzHEX    
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-"""
+    ABOUT_MSG = """നിങ്ങൾക് [donate](https://t.me/nokiyirunno_ippo_kittum) ചെയ്യാൻ താല്പര്യം ഉണ്ടെകിൽ എന്റെ അഡ്മിനുമായി സംസാരിക്കുക."""
