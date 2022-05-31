32c55924956a142e06b04e334a23fde42cc412dc
bgrmwbys <77823759+bgrmwbys@users.noreply.github.com>
2022-05-31 17:21:19 -0400

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -316,7 +316,7 @@ async def delallconfirm(client, message):
         )
 
 
-@Client.on_message(filters.group & filters.text)
+@Client.on_message(filters.group & filters.text & ~filters.edited)
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
