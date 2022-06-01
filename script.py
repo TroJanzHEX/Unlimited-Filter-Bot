c3cb29770cfe97d7efc4e657ab1cdb990ca1eb61
bgrmwbys <77823759+bgrmwbys@users.noreply.github.com>
2022-06-01 10:03:31 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ class Script(object):
 
     START_MSG = """<b>היי {},
 ברוך הבא לבוט הניהול של רובוטריק! 
-להצטרפות לקבוצה ↙️↙️ (http://t.me/robot_trick_Group)
+[להצטרפות לקבוצה ↙️↙️] (http://t.me/robot_trick_Group)
 T.me/robot_trick_Group
 
 הבוט רץ ע"י @Yishaicohen
